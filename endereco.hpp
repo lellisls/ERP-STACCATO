071b662dbe010be22e9fb4737036f3d1a3722670
Lucas <Lucaslellis777@gmail.com>
2015-03-20 13:38:43 -0300

    Adicionei uma classe "Endereco" para facilitar manipulação de endereços.

---
+++ endereco.hpp
@@ -0,0 +1,95 @@
+#ifndef ENDERECO_HPP
+#define ENDERECO_HPP
+
+#include <QSqlQuery>
+
+class Endereco {
+  explicit Endereco(int idEndereco);
+  QString m_descricao;
+  QString m_cep;
+  QString m_logradouro;
+  QString m_numero;
+  QString m_complemento;
+  QString m_bairro;
+  QString m_cidade;
+  QString m_uf;
+  bool m_ativo;
+  int m_idEndereco;
+
+public:
+  int idEndereco() const;
+  QString logradouro() const;
+  QString bairro() const;
+  QString cidade() const;
+  QString descricao() const;
+  void setDescricao(const QString & descricao);
+  QString cep() const;
+  void setCep(const QString & cep);
+  QString numero() const;
+  void setNumero(const QString & numero);
+  QString complemento() const;
+  void setComplemento(const QString & complemento);
+  QString uf() const;
+  void setUf(const QString & uf);
+  bool ativo() const;
+  void setAtivo(bool ativo);
+};
+
+#endif // ENDERECO_HPP
+
+
+Endereco::Endereco(int idEndereco) : m_idEndereco(-1) {
+  QSqlQuery qry("SELECT * FROM Endereco WHERE idEndereco = '" + QString::number(idEndereco) + "'");
+  if(!qry.exec() || !qry.first()) {
+    return;
+  }
+  m_idEndereco = idEndereco;
+  m_descricao = qry.value("descricao").toString();
+  m_cep = qry.value("cep").toString();
+  m_logradouro = qry.value("logradouro").toString();
+  m_numero = qry.value("numero").toString();
+  m_complemento = qry.value("complemento").toString();
+  m_bairro = qry.value("bairro").toString();
+  m_cidade = qry.value("cidade").toString();
+  m_uf = qry.value("uf").toString();
+  m_ativo = qry.value("ativo").toBool();
+}
+
+bool Endereco::ativo() const {
+  return m_ativo;
+}
+
+QString Endereco::uf() const {
+  return m_uf;
+}
+
+QString Endereco::complemento() const {
+  return m_complemento;
+}
+
+QString Endereco::numero() const {
+  return m_numero;
+}
+QString Endereco::cep() const {
+  return m_cep;
+}
+
+QString Endereco::descricao() const {
+  return m_descricao;
+}
+
+QString Endereco::cidade() const {
+  return m_cidade;
+}
+
+QString Endereco::bairro() const {
+  return m_bairro;
+}
+
+QString Endereco::logradouro() const {
+  return m_logradouro;
+}
+
+int Endereco::idEndereco() const {
+  return m_idEndereco;
+}

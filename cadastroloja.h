a4e23db6befb1e660112b3bc6f04064324f0d266
darktorres <darktorres@ig.com.br>
2015-05-22 17:38:44 -0300

    Implementado campo para escolher as pastas do ACBr

--- cadastroloja.h
+++ cadastroloja.h
@@ -18,8 +18,10 @@ class CadastroLoja : public RegisterDialog {
     void on_pushButtonAtualizar_clicked();
     void on_pushButtonCadastrar_clicked();
     void on_pushButtonCancelar_clicked();
+    void on_pushButtonEntradaNFe_clicked();
     void on_pushButtonNovoCad_clicked();
     void on_pushButtonRemover_clicked();
+    void on_pushButtonSaidaNFe_clicked();
 
     // methods derived from RegisterDialog
     void on_pushButtonBuscar_clicked();
@@ -28,37 +30,37 @@ class CadastroLoja : public RegisterDialog {
 
   public:
     /*!
- * \brief Utilizada para selecionar um item a partir de um QModelIndex
- * \param idx Índice do Model relacionado ao item, normalmente obtido ao clicar na tabela
- * \return
- */
+*\brief Utilizada para selecionar um item a partir de um QModelIndex
+*\param idx Índice do Model relacionado ao item, normalmente obtido ao clicar na tabela
+*\return
+*/
     virtual bool viewRegister(QModelIndex idx);
 
   private:
     /*!
- * \brief Função padrão para verificar campos obrigatórios
- * \return
- */
+*\brief Função padrão para verificar campos obrigatórios
+*\return
+*/
     virtual bool verifyFields(int row);
     /*!
- * \brief Onde ocorre o model.setData(), baseada nas informações da view.
- */
+*\brief Onde ocorre o model.setData(), baseada nas informações da view.
+*/
     virtual bool savingProcedures(int row);
     /*!
- * \brief Limpar os campos da tela
- */
+*\brief Limpar os campos da tela
+*/
     virtual void clearFields();
     /*!
- * \brief Função onde os mapeamentos são configurados
- */
+*\brief Função onde os mapeamentos são configurados
+*/
     virtual void setupMapper();
     /*!
- * \brief Função chamada para atualizar a view, escondendo botão atualizar, por exemplo
- */
+*\brief Função chamada para atualizar a view, escondendo botão atualizar, por exemplo
+*/
     virtual void registerMode();
     /*!
- * \brief Função chamada para atualizar a view, escondendo botão cadastrarm, por exemplo
- */
+*\brief Função chamada para atualizar a view, escondendo botão cadastrarm, por exemplo
+*/
     virtual void updateMode();
 
   private:

3b4e7213a869ee2a0e992be327994411b3d414b5
darktorres <darktorres@ig.com.br>
2015-05-27 18:27:38 -0300

    Dialog de validade na importação

---
+++ validadedialog.h
@@ -0,0 +1,29 @@
+#ifndef VALIDADEDIALOG_H
+#define VALIDADEDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+  class ValidadeDialog;
+}
+
+class ValidadeDialog : public QDialog
+{
+    Q_OBJECT
+
+  public:
+    explicit ValidadeDialog(QWidget *parent = 0);
+    ~ValidadeDialog();
+    int getValidade();
+
+  private slots:
+    void on_pushButtonSalvar_clicked();
+    void on_pushButtonCancelar_clicked();
+    void on_spinBox_valueChanged(int dias);
+    void on_dateEdit_dateChanged(const QDate &date);
+
+  private:
+    Ui::ValidadeDialog *ui;
+};
+
+#endif // VALIDADEDIALOG_H

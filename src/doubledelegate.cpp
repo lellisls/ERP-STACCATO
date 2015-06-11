a628fa716548dd56bd4da91eca4d78eac7218a50
darktorres <darktorres@ig.com.br>
2015-06-11 13:24:12 -0300

    DoubleDelegate modificado para usar QLocale pt-br

--- src/doubledelegate.cpp
+++ src/doubledelegate.cpp
@@ -5,9 +5,12 @@ DoubleDelegate::DoubleDelegate(QObject *parent) : QStyledItemDelegate(parent) {}
 DoubleDelegate::~DoubleDelegate() {}
 
 QString DoubleDelegate::displayText(const QVariant &value, const QLocale &locale) const {
+  Q_UNUSED(locale);
+  QLocale local;
+
   if (value.userType() == QVariant::Double) {
-    return locale.toString(value.toDouble(), 'f', 2);
+    return local.toString(value.toDouble(), 'f', 2);
   } else {
-    return QStyledItemDelegate::displayText(value, locale);
+    return QStyledItemDelegate::displayText(value, local);
   }
 }

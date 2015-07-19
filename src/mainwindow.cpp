3ed4e670f7f336aad828506ac3bbccf2d2720bfc
darktorres <darktorres@ig.com.br>
2015-07-19 20:17:57 -0300

    Tema escuro

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include <QShortcut>
 #include <QSettings>
+#include <QStyleFactory>
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
@@ -27,6 +28,28 @@
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
+
+//  qApp->setStyle(QStyleFactory::create("Fusion"));
+
+//  QPalette darkPalette;
+//  darkPalette.setColor(QPalette::Window, QColor(53,53,53));
+//  darkPalette.setColor(QPalette::WindowText, Qt::white);
+//  darkPalette.setColor(QPalette::Base, QColor(25,25,25));
+//  darkPalette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+//  darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+//  darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+//  darkPalette.setColor(QPalette::Text, Qt::white);
+//  darkPalette.setColor(QPalette::Button, QColor(53,53,53));
+//  darkPalette.setColor(QPalette::ButtonText, Qt::white);
+//  darkPalette.setColor(QPalette::BrightText, Qt::red);
+//  darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+
+//  darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+//  darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+//  qApp->setPalette(darkPalette);
+
+//  qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
 
   setWindowTitle("ERP Staccato");
   readSettings();
